26042950bf684a06a50ce955136b9f6ffd17cfa3
LOSIcat <98892734+LOSIcat@users.noreply.github.com>
2022-03-22 18:32:12 +0900

    220322 수정2
    1. 상하좌우
    2. 점프
    3. 1인칭 카메라

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 마우스를 움직인다
+// 카메라가 마우스에 따라 회전한다.
+public class CameraRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float rx;
+    float ry;
+    public float rotSpeed = 200;
+    // Update is called once per frame
+    void Update()
+    {
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+
+        rx = rx + my * rotSpeed * Time.deltaTime;
+        ry = ry + mx * rotSpeed * Time.deltaTime;
+
+        rx = Mathf.Clamp(rx, -70, 70);
+
+        transform.eulerAngles = new Vector3(-rx, ry, 0);
+    }
+}
--- Assets/Scenes/Script/PlayerMove.cs
+++ Assets/Scenes/Script/PlayerMove.cs
@@ -2,17 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// 사용자가 원하는 방향으로 이동하고싶다.
+// 사용자가 스페이스바를 누르면 점프하고싶다.
+// 2번까지 점프하고싶다.
+
+// PlayerMove: 사용자의 입력에 따라 상하좌우로 이동하고 싶다.
 public class PlayerMove : MonoBehaviour
 {
+    CharacterController cc;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+
+        cc = gameObject.GetComponent<CharacterController>();
+
+
+
     }
+
+    public float jumpPower = 6;
+    public float gravity = -9.8f;
+    float yVelocity;
+
+    int jumpCount;
+    public int maxJumpCount = 2;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+
+        // 점프를 하고싶다.
+        if (cc.isGrounded)
+        {
+            jumpCount = 0;
+
+        }
+        else
+        {
+            yVelocity += gravity * Time.deltaTime;
+        }
+
+
+        if (jumpCount < maxJumpCount && Input.GetButtonDown("Jump"))
+        {
+            yVelocity = jumpPower;
+            jumpCount++;
+        }
+
+
+
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+
+        transform.position = transform.position + dir * speed * Time.deltaTime;
+
+        dir = Camera.main.transform.TransformDirection(dir);
+        dir.y = 0;
+        dir.Normalize();
+        Vector3 velocity = dir * speed;
+
+        velocity.y = yVelocity;
+
+        cc.Move(velocity * Time.deltaTime);
+
     }
 }
